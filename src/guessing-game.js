770e233d7700971dd06bbe285ea49c51e542a713
temikartemoff <outlooking@icloud.com>
2023-02-24 14:01:47 +0400

    feat: add answer

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
+    
     constructor() {}
 
-    setRange(min, max) {
-
+    setRange(min, max){
+        this.max = max
+        this.min = min
     }
 
     guess() {
-
+        if (this.min+1 == this.max-1){
+            return this.min + 1
+        } else {
+            return Math.ceil((this.max+this.min)/2)
+        }
     }
 
     lower() {
-
+        return this.max=this.guess()
     }
 
     greater() {
-
+        return this.min=this.guess()
     }
 }
 
